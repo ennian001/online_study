93d45bf6d53de010e928bfb85a5899eff17fe38e
sunennian <sunennian@baiyaodajiankang.com>
2020-03-23 13:42:09 +0800

    线上教育：项目初始化

---
+++ src/main/java/com/ennian/onlinestudy/onlinestudy/OnlinestudyApplication.java
@@ -0,0 +1,13 @@
+package com.ennian.onlinestudy.onlinestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinestudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinestudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ennian/onlinestudy/onlinestudy/OnlinestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ennian.onlinestudy.onlinestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinestudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
